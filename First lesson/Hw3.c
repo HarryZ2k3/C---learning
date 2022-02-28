76bea244c803e7a582d3b7e925fa08e6396c091f
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-02-28 11:28:11 +0700

    completed version (backwards)

--- First lesson/Hw3.c
+++ First lesson/Hw3.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    int n;
+    int n, temp;
     printf("Enter your number: ");
     scanf("%d",&n);
-    while (n==0)
-        {
-            int temp;
-            temp = n%10;
-            printf("%d\t",temp);
-        }
-
+    while (n!=0)
+    {
+        temp = n %10;
+        n = n/10;
+        
+        printf("%d\t",temp);
+    }
     return 0;
 }
