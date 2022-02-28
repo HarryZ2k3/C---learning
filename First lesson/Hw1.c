8f2a161bb67504a60ce9a1444715f67b70b0c5db
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-02-28 10:44:53 +0700

    hw 11

---
+++ First lesson/Hw1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int v,a,u,t;
+    float s;
+    printf("a= \n");
+    scanf("%d",&a);
+    printf("u= \n");  
+    scanf("%d",&u);  
+    printf("t= \n");
+    scanf("%d",&t);
+    v = u + a*t;
+    s=  (float)u*t + (a*t*t)/2;
+    printf("The initial velocity of the object is %d\n",v);
+    printf("Distance travelled is %f",s);
+    
+    return 0;
+}
