886e350c0e572af702dfae0091453786a4811bc2
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-03-14 10:35:56 +0700

    bruh

---
+++ Third lesson/7.c
@@ -0,0 +1,13 @@
+//Huynh Phuong Dai - ITITWE21039
+#include <stdio.h>
+
+int main ()
+{
+    int sum =0;
+    for (int x=1;x ==13;++x)
+    {
+        sum +=x*x;
+        printf("%d\n",sum);
+    }
+    return 0;
+}
