a0efd587e2cabb3baebffa7fe944f3b1f6daa624
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-03-18 19:29:04 +0700

    q6

---
+++ HW1/Q6.c
@@ -0,0 +1,22 @@
+//Huynh Phuong Dai - ITITWE21039
+#include <stdio.h>
+
+int main()
+{
+    int defaultsal = 200;
+    double sales = 0, salary = 1;
+    int check;
+    while(salary !=0)
+    {
+        printf("Enter sales in dollars (-1 to end): ");
+        scanf("%lf",&sales);
+        if(sales == -1)
+        {
+            break;
+        }
+        salary = (defaultsal + (sales*9)/100);
+        printf("Salary is: $%.2lf\n",salary);
+        printf("\n");
+    }
+    return 0;
+}
