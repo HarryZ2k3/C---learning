e3ec8d7c0108c7b09158168977bbf26dc29b7ac4
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-03-18 21:27:45 +0700

    q8

---
+++ HW1/Q8.c
@@ -0,0 +1,69 @@
+//Huynh Phuong Dai - ITITWE21039
+#include <stdio.h>
+
+int main()
+{
+//a)
+    int x=5,y=8;
+    if (x==5)
+    {
+        if (y==8)
+        {
+            printf("@@@@@\n");
+        }
+        else
+        {
+            printf("#####\n");
+        }
+        printf("$$$$$\n");
+        printf("&&&&&\n"); 
+    }
+//b)
+    int y=8;
+    if (x==8)
+    {
+        if (y==5)
+        {
+            printf("@@@@@\n");
+        }
+        else
+        {
+            printf("#####\n");
+            printf("$$$$$\n");
+            printf("&&&&&\n"); 
+        }
+    return 0;
+    }
+//c)
+    int y=8;
+    if (x==8)
+    {
+        if (y==5)
+        {
+            printf("@@@@@\n");
+        }
+        else
+        {
+            printf("#####\n");
+            printf("$$$$$\n");
+        }
+        printf("&&&&&\n");
+    return 0;
+    }
+//d)
+    int y=7;
+    if (x==8)
+    {
+        if (y==5)
+        {
+            printf("@@@@@\n");
+        }
+        else
+        {
+            printf("#####\n");
+        }
+        printf("$$$$$\n");
+        printf("&&&&&\n");
+    return 0;
+    }
+}
