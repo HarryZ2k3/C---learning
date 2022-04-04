c5cb66a0142f23f2cc9caac09a63aee1c6e83b6e
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-04-04 07:53:06 +0700

    power code

--- 5th lesson/5.c
+++ 5th lesson/5.c
@@ -13,12 +13,12 @@ int main()
     printf("%d ^ %d = %d",base,exponent,intergerPower(base,exponent));
     return 0;
 }
-
 int intergerPower(int x,int y)
 {
     int power=1;
     for (int i = 1;i<y;++i)
     {
+        //run from 1 to less than y to times x y ammount of times
         power = power*x;
     }
     return power;
