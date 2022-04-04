cd2a3e915f681bdd529aa7019f69aec48470833d
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-04-04 08:48:31 +0700

    random num

---
+++ 6th lesson/2.c
@@ -0,0 +1,13 @@
+//Huynh phuong dai - ititwe21039
+//this code demostrates the use of random number generator
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double i;
+    i = rand();
+    printf("%f",i);
+    return 0;
+}
