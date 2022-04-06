0ec2e55da97156ec272ec73aaf34a144a6ddc082
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-04-06 19:12:05 +0700

    do..while vs while loops

--- HW2/Q2.c
+++ HW2/Q2.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-
+    int i=0;
+    do
+    {
+        //this do...while loop print out the numbers from 0 to 4
+        printf("%i\t",i);
+        i++;
+    } while (i<5);
+    i=0;
+    //transform to while loop only:
+    printf("\n");
+    while (i<5)
+    {
+        printf("%i\t",i);
+        i++;
+    }
+        //same premise, different loop
     return 0;
 }
