fc2822a6b019a9a6d3591dc04bf7ca6088fdf18d
Huỳnh Phương Đại <61302784+HarryZ2k3@users.noreply.github.com>
2022-04-14 13:10:45 +0700

    lab2 setup

---
+++ Phuong_dai_lab02/Q1.c
@@ -0,0 +1,13 @@
+//ITITWE21039_Huynh Phuong Dai
+
+#include <stdio.h>
+int GetSum (int)
+
+int main()
+{   
+    int a;
+    printf("Enter a interger: ");
+    scanf("%d",&a);
+
+    return 0;
+}
---
+++ Phuong_dai_lab02/Q5.C
@@ -0,0 +1,9 @@
+//ITITWE21039_Huynh Phuong Dai
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
