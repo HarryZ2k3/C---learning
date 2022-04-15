0b86bef58f94cd690d67d0a20aa89eaf79ef5f91
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-04-15 20:43:02 +0700

    q1 answered

--- HW3/Q1.C
+++ HW3/Q1.C
@@ -1,8 +1,38 @@
 //ITITWE21039 - HUYNH PHUONG DAI
+#include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
+//a fix
+double cube(float);
 
-    return 0;
+double cube(float number)
+{   
+    return number * number * number;
 }
+
+//b fix
+int randomNumber = rand();
+
+//c fix 
+double y = 123.45678;
+double x;
+x = y;
+printf("%f\n", x);
+
+//d fix
+double square(double number)
+{
+    return number * number;
+}
+
+//e fix
+int sum(int n)
+{
+if (0 == n) {
+    return 0;
+    }
+    else {
+        return n + sum(n-1);
+    }
+}
+
