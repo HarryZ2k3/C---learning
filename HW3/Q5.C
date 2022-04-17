0d44c1b29b5684e252e0bda43ea3acb9f8864c6c
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-04-17 20:09:12 +0700

    Q5 done

--- HW3/Q5.C
+++ HW3/Q5.C
@@ -1,8 +1,6 @@
 //ITITWE21039 - HUYNH PHUONG DAI
-#include <stdio.h>
 
-int main()
-{
+//a Read the 20 elements of double array sales from the keyboard
     double sales[20];//array declaration
     int i;
     printf("Enter sales data: \n");// use input
@@ -12,9 +10,20 @@ int main()
     }
 
     printf("The contentes of sales are: \n");
-    for(i=0;i<20;i++)//print out the variable in the array
-    {
-        printf("%.2lf ",sales[i]);
-    }
-    return 0;
-}
+
+// bAdd 1000 to each of the 75 elements of double array allowance.
+    for (i=0; i<75;++i)
+    Allowance[i]=Allowance[i]+1000; 
+
+//c Initialize the 50 elements of integer array numbers to zero.
+    for (i=0; i<50;++i)
+    numbers[i]=0;
+
+//d  Print the 10 values of integer array GPA in column format.
+    double GPA[10]={3.5,2,3.0,1,2.4,4,4.87,4.5,4,4.8};
+    int i;
+    printf("GPA\n");
+    for (i=0; i<10;++i)
+    printf("%f\n",GPA[i]);
+
+
