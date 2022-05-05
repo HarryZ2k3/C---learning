1e3198f44a579e08d8b17a08e754d5c2034092b0
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-05-05 19:24:58 +0700

    LAB 4 finished

--- LAB/04/Q5.C
+++ LAB/04/Q5.C
@@ -1,13 +1,25 @@
 //ITITWE21039 HUYNNH PHUONG DAI 
+/*
+Algorithm works by intializing 2 identical size arrays, one for input, one for output
+Using for loop with a stopping condition, we enter the input array.
+The insert and remove functions works by calling in 2 arrays, the index number and the number to replace.
+
+Insert function: Assign variables of each adress of input array to output array until the address hits the index number, then replace the variable of the index adress of the input array 
+to the number to replace, then assign the variable of input array as normal but shift to the right (+1) the output array's adress to make room for the newly inserted variable
+
+Remove function: Assign variables of each adress of input array to output array until the address hits the index number,
+then assign the variable of input array as normal but shift to the right (+1) the input array's adress to ignore the variable in the index adress to remove it in the final output
+
+*/
 #include <stdio.h>
 void insert(int arr1[],int a,int b,int arr2[]);
 void remove(int arr1[],int a,int b,int arr2[]);
 int main ()
 {
-    int A[30],B[30],id;
+    int A[30]={0},B[30]={0},id;
     printf("Enter the index number:\n");
     scanf("%d",&id);
-    printf("Enter the numbers in the array: (-1 to stop)");
+    printf("Enter the numbers in the array: (-1 to stop)\n");
     for (int i =0;i<30;i++)
     {
         scanf("%d",&A[i]);
@@ -17,34 +29,29 @@ int main ()
         }
     }
     insert(A,id,5,B);
+    printf("\n");
     remove(A,id,3,B);
     return 0;
 }
 void insert(int arr1[],int a,int b,int arr2[])
 {
-    for (int i=0;i<30;i++)
+    //insert
+    a-=1;
+    for (int i=0;i<a;i++)
     {
-        arr2[i]=arr1[i];
-        if (i==a)
-        {
-            arr2[i]=b;
-            break;
-        }
+        arr2[i]=arr1[i];   
     }
-    for (int i=a+1;i<30;i++)
+    arr2[a]=b;
+    for (int i=a;i<30;i++)
     {
-        if (arr1[i]==0)
-        {
-            break;
-        }
-        else 
-            arr2[i]=arr1[i];
+        arr2[i+1]=arr1[i];
     }
+    //print
     for (int i = 0; i<30;i++)
     {
-        if (arr2[i]==0)
+        if (arr2[i+1]==-1)
         {   
-            printf("]");
+            printf("%d]",arr2[i]);
             break;
         }
         else if(i==0)
@@ -56,28 +63,23 @@ void insert(int arr1[],int a,int b,int arr2[])
 
 void remove(int arr1[],int a,int b,int arr2[])
 {
-    for (int i=0;i<30;i++)
+    a-=1;
+    //remove
+    for (int i=0;i<a;i++)
     {
-        arr2[i]=arr1[i];
-        if (i==a)
-        {
-            break;
-        }
+        arr2[i]=arr1[i];   
     }
-    for (int i=a+1;i<30;i++)
+    for (int i=a;i<30;i++)
     {
-        if (arr1[i]==0)
-        {
-            break;
-        }
-        else 
-            arr2[i]=arr1[i];
+        arr2[i]=arr1[i+1];
     }
+    //print
     for (int i = 0; i<30;i++)
     {
-        if (arr2[i]==0)
+        if (arr2[i+1]==-1)
         {   
-            printf("]");
+            printf("%d]",arr2[i]);
+
             break;
         }
         else if(i==0)
