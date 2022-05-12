039feefefb16f7e03373ee5d754080dc805c1cc5
Harry Phuong Dai's Laptop <phuongdai.saigon@gmail.com>
2022-05-12 13:48:37 +0700

    q1 finish

--- LAB/05/01.c
+++ LAB/05/01.c
@@ -4,32 +4,38 @@
 //Initialize 3 arrays, 1 empty 2D array as the product.
 // Calculate by taking the dot product of the each row of the first array and the second vetor array
 // assign the the results in the first and second row of the 3rd empty array
+//then print the array out
 int main()
 {
     int arr1[2][3]=
     {{1,2,3},
      {4,5,6}};
     int arr2[3]={7,8,9};
-    int arr3[2][3]=0;
-    for (int i;i<2;i++)
+    int arr3[2][3]={0};
+    for (int i=0;i<2;i++)
     {
-        for (int j;k<3;j++)
+        for (int j=0;j<3;j++)
         {
-            arr3[i][j]=arr1[i][j]*arr[j];
+            arr3[i][j]=arr1[i][j]*arr2[j];
         }
     }
+    printf("Answer is:\n");
     for (int i=0;i<2;i++)
     {
         for (int j=0;j<3;j++)
         {
             if (i==0&&j==0)
-                printf("C={&d;",arr3[i][j]);
-            if (i==1&&j==2)
+                printf("C={%d;",arr3[i][j]);
+            else if (i==1&&j==2)
                 printf("%d}",arr3[i][j]);
-            if (i==1&&j==0)
-                printf("}{%d;",arr3[i][j]);
+            else if (i==1&&j==0)
+                printf("{%d;",arr3[i][j]);
+            else if (i==0&&j==2)
+            {
+                printf("%d}",arr3[i][j]);
+            }
             else
-                printf("%d;",arr3[i][j])
+                printf("%d;",arr3[i][j]);
         }
     }
 
