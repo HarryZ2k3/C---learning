8c3a30c1663447959e362838819ea67ea0360529
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-05-16 08:34:49 +0700

    1 2

--- 8th lesson/1.c
+++ 8th lesson/1.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-
+    printf("original \t pointer\n");
+    int a=7;
+    int *aprt;
+    aprt = &a;
+    printf("before\n%d \t\t %d",a,*aprt);
+    *aprt = 9;
+    printf("\nafter\n%d \t\t %d",a,*aprt);
     return 0;
 }
