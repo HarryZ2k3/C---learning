b11aab96eef0d93a238a049d392e5d763df21219
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-05-23 08:33:36 +0700

    yes

--- 9th lesson/1.c
+++ 9th lesson/1.c
@@ -1,8 +1,14 @@
 //Huynh Phuong Dai - ITITWE21039    
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-
+    char a[30];
+    scanf("%s",a);
+    if (a[0] == 'T' && a[1] == 'h')
+        printf("true");
+    else 
+        printf("false");
+    printf("\n%s",a);
     return 0;
 }
--- 9th lesson/2.c
+++ 9th lesson/2.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-
+    char a[30];
+    gets(a);
+    if (a[0] == 'T' && a[1] == 'h')
+        printf("true");
+    else 
+        printf("false");
+    printf("\n%s",a);
     return 0;
 }
