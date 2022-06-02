2b710639a4a0b776430a2c299db447eb1aec1e6a
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-06-02 21:22:30 +0700

    question 4,5 finished

--- LAB/07/4.c
+++ LAB/07/4.c
@@ -1,9 +1,35 @@
 //Huynh Phuong Dai - ITITWE21039
 #include <stdio.h>
-#include <string.h>
+//program is simply goest through all of the words in a sentence input and check for the things needed
+int main() {
+    char line[150];
+    int vowels, consonant, digit, space;
+    vowels = consonant = digit = space = 0;
+    printf("Enter a line of string: ");
+    fgets(line, sizeof(line), stdin);
+    for (int i = 0; line[i] != '\0'; ++i) {
+        line[i] = tolower(line[i]);
+        //check for vowels
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u') {
+            ++vowels;
+        }
+        //checks words (that is not a vowels)
+        else if ((line[i] >= 'a' && line[i] <= 'z')) {
+            ++consonant;
+        }
+        //check digits
+        else if (line[i] >= '0' && line[i] <= '9') {
+            ++digit;
+        }
+        else if (line[i] == ' ') {
+            ++space;
+        }
+    }
 
-int main()
-{
-    
+    printf("Vowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
+
     return 0;
 }
--- LAB/07/5.c
+++ LAB/07/5.c
@@ -4,6 +4,34 @@
 
 int main()
 {
-    
+    char str[100], word[20],stringcpy[20];
+    int i, j, ls, lw, temp, chk=0;
+    printf("Enter the String: ");
+    gets(str);
+    printf("Enter a Word: ");
+    gets(word);
+    strcpy(stringcpy,word);
+    ls = strlen(str);
+    lw = strlen(word);
+    for(i=0; i<ls; i++)
+    {
+        temp = i;
+        for(j=0; j<lw; j++)
+        {
+            if(str[i]==word[j])
+                i++;
+        }   
+        chk = i-temp;
+        if(chk==lw)
+        {
+            i = temp;
+            for(j=i; j<(ls-lw); j++)
+                str[j] = str[j+lw];
+            ls = ls-lw;
+            str[j]='\0';
+        }
+    }    
+    printf("\nNew String = %s", str);
+    getch();
     return 0;
 }
