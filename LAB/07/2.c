e3a497766a061fabaf9ec4c485410c30ba82f406
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-06-02 20:34:53 +0700

    lab 7 question 2done

--- LAB/07/2.c
+++ LAB/07/2.c
@@ -2,68 +2,115 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define SIZE 5
 
-
-struct Person 
+typedef struct
 {
     char ID[5];
     char firstname[21];
     char lastname[21];
     int age;
-    int phone[10];
-};
-void IDsearch(struct Person pp[5])
+    char phone[10];
+}Person;
+
+void IDsearch(Person *pp,int size);
+void printstaffinfo(Person *pp,int index);
+void HighestAge(Person *pp);
+void LowestAge(Person *pp);
+
+int main()
+{
+    Person pp[SIZE];
+    //5 staff member information enter
+    for (int i = 0; i<SIZE;i++)
+    {
+        printf("Employee number %d\n",i+1);
+        printf("Input Their First name: ");
+        scanf("%s",pp[i].firstname);
+        printf("Input Their Last name: ");
+        scanf("%s",pp[i].lastname);
+        printf("Input Their staff ID: ");
+        scanf("%s",pp[i].ID);
+        printf("Enter their age: ");
+        scanf("%d",&pp[i].age);
+        printf("Enter their phone number: ");
+        scanf("%s",pp[i].phone);
+        
+    }
+    //Staff search via ID
+    IDsearch(pp,SIZE);
+    //
+    //Lowest/highest
+    HighestAge(pp);
+    LowestAge(pp);
+
+    return 0;
+}
+
+void IDsearch(Person *pp,int size)
 {
     printf("Enter ID: \n");
     char checkID[5];
     scanf("%s",checkID);
     int check=0;
-    for (int i=0;i<5;i++)
+    for (int i=0;i<SIZE;i++)
     {
-        for(int j=0;j<5;j++)
+        for (int j=0;j<5;j++)
         {
-            if (pp[i].ID[j] == checkID[j])
+            if (checkID[j] == pp[i].ID[j])
+            {
                 check++;
+            }
+            else
+                check=0;
         }
-        if (check ==5)
+        if (check==0)
         {
-            printf("ID: %s\n",pp[i].ID);
-            printf("First name: %s\n",pp[i].firstname);
-            printf("Last name: %s\n",pp[i].lastname);
-            printf("Age: %d\n",pp[i].age);
-            printf("Phone number %s\n",pp[i].phone);
-        }
-        else
-        {
-            check=0;
             continue;
         }
+        else 
+            printstaffinfo(pp,i);
     }
 }
-int main()
+
+
+void printstaffinfo(Person *pp,int index)
 {
-    struct Person pp[5];
-    //5 staff member information enter
-    for (int i = 0; i<5;i++)
+    printf("FIRST NAME: %s\n",pp[index].firstname);
+    printf("LAST NAME: %s\n",pp[index].lastname);
+    printf("ID: %s\n",pp[index].ID);
+    printf("AGE: %d\n",pp[index].age);
+    printf("PHONE NUMBER: %s\n",pp[index].phone);
+}
+
+void HighestAge(Person *pp)
+{
+    int index=0;
+    int highest = pp[0].age;
+    for (int i=1;i<SIZE;i++)
     {
-        printf("\nInput The First Name of staff member number %d: ",i+1);
-        scanf("%s",&pp[i].firstname);
-        printf("\nInput Their Last name: ");
-        scanf("%s",&pp[i].lastname);
-        printf("\nInput Their staff ID (with spaces between the characters): ");
-        scanf("%s",pp[i].ID);
-        printf("\nEnter their age: ");
-        scanf("%d",&pp[i].age);
-        printf("\nEnter their phone number: ");
-        for (int j=0;j<10;j++)
+        if (highest < pp[i].age)
         {
-            scanf("%d",&pp[i].phone[j]);
+            highest = pp[i].age;
+            
         }
     }
-    //Staff search via ID
-    IDsearch(pp);
-    //
+    printf("Highest age is %s at %d years old\n",pp[index].firstname,highest);
 
-    return 0;
 }
 
+void LowestAge(Person *pp)
+{
+    int index=0;
+    int lowest = pp[0].age;
+    for (int i=1;i<SIZE;i++)
+    {
+        if (lowest > pp[i].age)
+        {
+            lowest = pp[i].age;
+            index = i;
+        }
+    }
+    printf("Lowest age is %s at %d years old\n",pp[index].firstname,lowest);
+
+}
